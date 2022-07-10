c0db9fddc05b21fca595e06aaf5b463f420ddeaf
Alexis Yusti <alexisyusti18@gmail.com>
2022-07-10 19:57:14 -0300

    a

--- frontend/src/redux/actions/commentsActions.js
+++ frontend/src/redux/actions/commentsActions.js
@@ -7,9 +7,7 @@ const commentsActions={
         const token= localStorage.getItem('token')
         //console.log(token)
         return async(dispatch, getState)=>{
-            //SI EL COMENTARIO NO ESTA VACIO=> QUE EL CAPO A CARGAR TENGA ALGO
             if(data.comment !== "") {
-                //SI COMMENT NO ESTA VACIO, SE HACE EL PEDIDO LA RUTA
                 const res= await axios.post(`${url}/api/comment`, {data},{
                     headers:{
                         'Authorization':'Bearer '+token
@@ -25,12 +23,11 @@ const commentsActions={
                 })
                 return res
             } else{
-                //EN CASO DE QUE ESTE VACIO EL CAMPO, DEVUELVO EL MENSAJE
                 dispatch({
                     type:'MESSAGE',
                     payload:{
                         view:true,
-                        message:'Enter a comment before sending',//Ingresa un comentario antes de enviar
+                        message:'Enter a comment before sending'
                     }
                 })
             }
