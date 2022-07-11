3018006f0f91e53b1c775ed45da5f3dd32fefe1d
Alexis Yusti <alexisyusti18@gmail.com>
2022-07-10 21:25:34 -0300

    a

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Router= require('./routes/routes')
 const cors= require('cors') 
 const passport= require('passport') //REQUIERO PASSPORT DEL PAQUETE PASSPORT
 
-const PORT=4000
+const PORT = process.env.PORT || 4000
 const app= express() //ejecuto express para crear una app
 //MIDDLEWARES
 app.use(express.json()) 
@@ -13,8 +13,14 @@ app.use(passport.initialize()) //LLAMO A INICIALIZAR A PASSPORT COMO MIDLLEWARE 
 app.use(cors()) 
 app.use('/api', Router)
 
+app.set('port', PORT)
+
+app.get ('/', (req, res) => {
+    res.send('SERVER CREATE')
+})
+
 app.listen(PORT, ()=>{
-    console.log('SERVER READY ON PORT:' + PORT);
+    console.log('SERVER READY ON PORT:' + app.get('port'));
 })
 
 
